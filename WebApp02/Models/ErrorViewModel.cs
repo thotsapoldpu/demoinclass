ec410fe2d13b26afce195d47abd80925abe8a11e
thossapol.api <thossapol.api@dpu.ac.th>
2017-11-20 14:03:16 +0700

    Initial git

---
+++ WebApp02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApp02/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApp02.Models;
+
+namespace WebApp02.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApp02.Models.MovieContext _context;
+
+        public IndexModel(WebApp02.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
